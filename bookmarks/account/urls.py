d0234c258e49f9be4e1f748c85e4f6edd2329f83
desirelesson <theproblemdi@gmail.com>
2023-11-24 18:53:35 +0300

    added path for the password_change/password_change_done in account/urls.py

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -6,8 +6,12 @@ from . import views
 
 urlpatterns = [
     #path('login/', views.user_login, name='login'),
+    # url-address for login/logout
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # url-address for the change password
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 
     path('', views.dashboard, name='dashboard'),
 
