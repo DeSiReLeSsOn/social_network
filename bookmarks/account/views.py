3c2b8ba84deeb610a06ecf8d227a0194b52a6018
desirelesson <theproblemdi@gmail.com>
2023-11-27 20:04:30 +0300

    fixed edit method

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -54,10 +54,11 @@ def register(request):
 def edit(request):
     if request.method == 'POST':
         user_form = UserEditForm(instance=request.user, data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST)
-    if user_form.is_valid() and profile_form.is_valid():
-        user_form.save()
-        profile_form.save()
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
